042922e54d878346d9d3b9d0fd7ec612fe34a6c1
Bogdan Kiptenko <bogdankiptenko@gmail.com>
2021-06-10 15:40:15 +0300

    Added exception handling for NullPointerException in DocxGenerator (#10)

--- src/main/java/ua/edu/sumdu/j2ee/kiptenko/demo/converter/DocxGenerator.java
+++ src/main/java/ua/edu/sumdu/j2ee/kiptenko/demo/converter/DocxGenerator.java
@@ -1,9 +1,6 @@
 package ua.edu.sumdu.j2ee.kiptenko.demo.converter;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.URISyntaxException;
 
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
@@ -15,27 +12,40 @@ import ua.edu.sumdu.j2ee.kiptenko.demo.model.NewsPojo;
 public class DocxGenerator {
     public static byte[] generateTemplate(NewsPojo nptest) throws IOException, URISyntaxException {
         XWPFDocument document = new XWPFDocument();
-        FileOutputStream out = new FileOutputStream(new File(DocxGenerator.class.getClassLoader().getResource("template.docx").toURI()));
+        File template;
+        FileOutputStream out;
+        try{
+            template = new File(DocxGenerator.class.getClassLoader().getResource("template.docx").toURI());
+            if(!template.exists()){
+                template.createNewFile();
+            }else{
+                System.out.println("File already exists");
+            }
+            out = new FileOutputStream(template);
 
-        XWPFParagraph paragraph = document.createParagraph();
-        XWPFRun run = paragraph.createRun();
-        run.setFontSize(14);
-        run.setFontFamily("TimesNewRoman");
-        for(int i = 0; i < nptest.getSources().length; i++) {
-            run.setText("\nShort Description: " + nptest.getSources()[i].getDescription());
-            run.addCarriageReturn();
-            run.setText("\nOriginal Link: " + nptest.getSources()[i].getUrl());
-            run.addCarriageReturn();
-            run.setText("\nCategory: " + nptest.getSources()[i].getCategory());
-            run.addCarriageReturn();
-            run.setText("\nCountry: " + nptest.getSources()[i].getCountry());
-            run.addCarriageReturn();
-            run.setText("\nLanguage: " + nptest.getSources()[i].getLanguage());
-            run.addCarriageReturn();
-            run.addCarriageReturn();
+            XWPFParagraph paragraph = document.createParagraph();
+            XWPFRun run = paragraph.createRun();
+            run.setFontSize(14);
+            run.setFontFamily("TimesNewRoman");
+            for(int i = 0; i < nptest.getSources().length; i++) {
+                run.setText("\nShort Description: " + nptest.getSources()[i].getDescription());
+                run.addCarriageReturn();
+                run.setText("\nOriginal Link: " + nptest.getSources()[i].getUrl());
+                run.addCarriageReturn();
+                run.setText("\nCategory: " + nptest.getSources()[i].getCategory());
+                run.addCarriageReturn();
+                run.setText("\nCountry: " + nptest.getSources()[i].getCountry());
+                run.addCarriageReturn();
+                run.setText("\nLanguage: " + nptest.getSources()[i].getLanguage());
+                run.addCarriageReturn();
+                run.addCarriageReturn();
+            }
+            document.write(out);
+            out.close();
+        } catch (NullPointerException | FileNotFoundException e){
+            System.out.println(e);
         }
-        document.write(out);
-        out.close();
+
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
         try {
             document.write(stream);
